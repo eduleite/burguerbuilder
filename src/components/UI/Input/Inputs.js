75fb27fed5b391466f56c38b34fb9748d55a7d89
edule <edu.leite@gmail.com>
2018-07-15 20:22:34 -0300

    Parei na lição 237, iniciando lição sobre Redux

--- src/components/UI/Input/Inputs.js
+++ src/components/UI/Input/Inputs.js
@@ -3,9 +3,16 @@ import classes from './Input.css';
 
 const input = (props) => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+    let validationError = null;
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+        validationError = <p>Please enter a valid value!</p>;
+    }
+
     switch(props.elementType) {
         case ('input'):
-            inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
+            inputElement = <input className={inputClasses.join(' ')} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
             break;
         case ('textarea'):
             inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} onChange={props.changed}/>;
@@ -24,6 +31,7 @@ const input = (props) => {
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     );
 };
