712e63c97d7f6e439af231826c5a23ccc11e9583
edule <edu.leite@gmail.com>
2018-10-26 20:21:32 -0300

    Faltou converter a última classe para react-hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import Layout from './hoc/Layout/Layout';
 import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
@@ -7,52 +7,50 @@ import Logout from "./containers/Auth/Logout/Logout";
 import { authCheckState } from "./store/actions/index";
 import * as asyncComponents from './components/AsyncComponents';
 
-class App extends Component {
+function App(props) {
 
-    componentDidMount() {
-        this.props.onTryAutoSignup();
-    }
+    React.useEffect(() => {
+        props.onTryAutoSignup();
+    }, []);
 
-    render() {
-        let routes = (
+    let routes = (
+        <Switch>
+            <Route path="/auth" component={asyncComponents.AuthAsync}/>
+            <Route exact path="/" component={BurguerBuilder}/>
+            <Redirect to="/"/>
+        </Switch>
+    );
+    if (props.isAuthenticated) {
+        routes = (
             <Switch>
+                <Route path="/checkout" component={asyncComponents.CheckoutAsync}/>
+                <Route path="/orders" component={asyncComponents.OrdersAsync}/>
+                <Route path="/logout" component={Logout}/>
                 <Route path="/auth" component={asyncComponents.AuthAsync}/>
                 <Route exact path="/" component={BurguerBuilder}/>
                 <Redirect to="/"/>
             </Switch>
         );
-        if (this.props.isAuthenticated) {
-            routes = (
-                <Switch>
-                    <Route path="/checkout" component={asyncComponents.CheckoutAsync}/>
-                    <Route path="/orders" component={asyncComponents.OrdersAsync}/>
-                    <Route path="/logout" component={Logout}/>
-                    <Route path="/auth" component={asyncComponents.AuthAsync}/>
-                    <Route exact path="/" component={BurguerBuilder}/>
-                    <Redirect to="/"/>
-                </Switch>
-            );
-        }
-        return (
-            <div>
-              <Layout>
-                  {routes}
-              </Layout>
-            </div>
-        );
     }
+    return (
+        <div>
+            <Layout>
+                {routes}
+            </Layout>
+        </div>
+    );
 }
 
-const mapStateToProps = state => {
+function mapStateToProps(state) {
     return {
         isAuthenticated: state.auth.token !== null
-    }
-};
+    };
+}
 
-const mapDispatchToProps = dispatch => {
+function mapDispatchToProps(dispatch) {
     return {
         onTryAutoSignup: () => dispatch(authCheckState())
     };
-};
+}
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
